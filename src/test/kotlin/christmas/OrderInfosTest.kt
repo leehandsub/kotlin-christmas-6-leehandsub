0f0247ad277207b0bcff1d91df0dc53ef5e42788
leehandsub <gustjq7027@naver.com>
2023-11-15 21:02:50 +0900

    test : 메뉴입력 예외 상황 테스트

---
+++ src/test/kotlin/christmas/OrderInfosTest.kt
@@ -0,0 +1,30 @@
+package christmas
+
+import christmas.model.OrderInfos
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class OrderInfosTest {
+
+    @Test
+    fun `정해진 메뉴가 아닐 때 테스트`() {
+        assertThrows<IllegalArgumentException> {
+            OrderInfos(listOf(listOf("국밥", "1"), listOf("제로콜라", "1")))
+        }
+    }
+
+    @Test
+    fun `음료만 주문 했을 때 테스트`() {
+        assertThrows<IllegalArgumentException> {
+            OrderInfos(listOf(listOf("제로콜라", "1"), listOf("샴페인", "1")))
+        }
+    }
+
+    @Test
+    fun `메뉴가 20개 넘었을 때 테스트`() {
+        assertThrows<IllegalArgumentException> {
+            OrderInfos(listOf(listOf("초코케이크", "21")))
+        }
+    }
+
+}
