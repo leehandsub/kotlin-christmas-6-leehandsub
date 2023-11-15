d406dbb2e8dbb61c7c38868d676a83ebb33a49e5
leehandsub <gustjq7027@naver.com>
2023-11-15 15:30:38 +0900

    feat : 날짜입력 예외 설정

--- src/main/kotlin/christmas/view/InputView.kt
+++ src/main/kotlin/christmas/view/InputView.kt
@@ -8,6 +8,8 @@ object InputView {
     private const val MENU_INPUT_ANNOUNCE_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"
     private const val DELIMITER_COMMA = ","
     private const val DELIMITER_DASH = "-"
+    private const val ERROR_NUMBER_MESSAGE = "입력값이 숫자가 아닙니다."
+    private const val ERROR_DAY_RANGE_MESSAGE = "유효하지 않은 날짜입니다. 다시 입력해 주세요."
 
     fun printGreeting() {
         println(GREETING_MESSAGE)
@@ -15,9 +17,9 @@ object InputView {
 
     fun getInputDay(): Int {
         println(CHRISTMAS_DAY_MESSAGE)
-        val inputDay = Console.readLine();
-
-        return inputDay.toInt()
+        val inputDay = checkPositiveInteger(Console.readLine())
+        checkNumberRange(inputDay)
+        return inputDay
     }
 
     fun getInputOrderMenu(): MutableList<List<String>> {
@@ -25,5 +27,23 @@ object InputView {
         return Console.readLine().split(DELIMITER_COMMA).map {
             it.split(DELIMITER_DASH)
         }.toMutableList()
+    }
+
+    private fun checkPositiveInteger(inputWinningNumber: String): Int {
+        val winningNumber = inputWinningNumber.toIntOrNull() ?: 0
+        require(winningNumber > 0) {
+            errorMessageFormat(ERROR_NUMBER_MESSAGE)
+        }
+
+        return winningNumber
+    }
+    private fun checkNumberRange(inputDay: Int) {
+        check(inputDay <= 31) {
+            errorMessageFormat(ERROR_DAY_RANGE_MESSAGE)
+        }
+    }
+
+    fun errorMessageFormat(errorMessage: String): String {
+        return "[ERROR] %s".format(errorMessage)
     }
 }
